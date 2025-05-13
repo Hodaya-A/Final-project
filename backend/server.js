37424c365f32a712c1cf760c551e3c4dbbd0977f
Hodaya-A <hodaya3avraham@gmail.com>
2025-05-13 12:41:07 +0300

    rename

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,8 @@ app.use(cors())
 app.use(express.json())
 
 // חיבור ל־MongoDB (מסד מקומי, אפשר לשנות ל-Atlas)
-mongoose.connect('mongodb://localhost:27017/freshend', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ Connected to MongoDB'))
-.catch((err) => console.error('❌ MongoDB connection error:', err))
 
-const Product = require('./models/Product')
+mongoose.connect('mongodb://localhost:27017/freshend')
 
 // API – החזרת כל המוצרים
 app.get('/api/products', async (req, res) => {
