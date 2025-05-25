d87a6c14f60f6bc2779729910ad2407081f1bff5
aviva <avivabr@post.jce.ac.il>
2025-05-25 13:10:14 +0300

    Product page update

--- frontend/src/components/ProductCard.vue
+++ frontend/src/components/ProductCard.vue
@@ -3,12 +3,17 @@
     <h3>{{ product.name }}</h3>
     <p>מחיר: ₪{{ product.price }}</p>
     <p>פג תוקף: {{ formattedDate }}</p>
+    <button @click="addToCart">הוסף לסל 🛒</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useCartStore } from '@/stores/cart'
+const cartStore = useCartStore()
+
 const props = defineProps<{
   product: {
+    _id: string
     name: string
     price: number
     expiryDate: string
@@ -16,19 +21,38 @@ const props = defineProps<{
 }>()
 
 const formattedDate = new Date(props.product.expiryDate).toLocaleDateString('he-IL')
+
+function addToCart() {
+  cartStore.addToCart({
+    id: props.product._id,
+    name: props.product.name,
+    price: props.product.price
+  })
+}
 </script>
 
 <style scoped>
 .card {
-  border: 1px solid #ccc;
+  border: 1px solid #d02525;
   border-radius: 8px;
   padding: 1rem;
-  background-color: #f9f9f9;
+  background-color: #df1c1c;
   text-align: center;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
-
 .card h3 {
   margin: 0 0 0.5rem;
+}
+button {
+  margin-top: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #45a049;
 }
 </style>
