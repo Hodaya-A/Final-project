80a8a63cf318adb2f90454b2fcd6bd7243559c63
Hodaya-A <hodaya3avraham@gmail.com>
2026-01-13 13:10:24 +0200

    Deleting an example env and deleting logs from the server.js

--- backend/server.js
+++ backend/server.js
@@ -99,7 +99,6 @@ app.use("/uploads", express.static("uploads"));
 app.use("/uploads/images", express.static("uploads/images"));
 
 /* ======================= MongoDB ======================= */
-console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI)
   .catch((err) => console.error("❌ שגיאה בחיבור למונגו:", err));
@@ -269,5 +268,3 @@ setInterval(checkExpiredOrders, ONE_MINUTE);
 
 // הרץ מיד בהפעלת השרת
 checkExpiredOrders();
-
-console.log("Environment Variables:", process.env);
